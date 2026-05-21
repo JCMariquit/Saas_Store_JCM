7fc39c7d895f19facb55c9c38b18866f2fdc5c9c
June Charles Mariquit <junecharlesmariquit553@gmail.com>
2026-05-21 16:56:00 +0800

    web routes

--- jcm-inventory_pos/routes/web.php
+++ jcm-inventory_pos/routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth'])->group(function () {
-
+ 
     Route::get('/dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
